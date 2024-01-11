b9ba81225312bb4fc894876169dd2e6ec013b838
c99av <aylloncristian99@gmail.com>
2024-01-11 15:42:42 +0100

    MediaEntreDosNumeros

---
+++ 02 - TiposPrimitivosyVariables/mediaentredosnumeros.java
@@ -0,0 +1,35 @@
+ 
+    import java.util.Scanner;
+
+public class mediaentredosnumeros {
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    //Declaración de variable
+    int num1;
+    int num2;
+    int suma;
+    double media;
+
+    //Solicitamos el primer número y la guardamos en una varible
+    System.out.println("Introduce un número");
+    num1 = sc.nextInt();
+
+    //Solicitamos el primer número y la guardamos en una varible
+    System.out.println("Introduce otro número");
+    num2 = sc.nextInt();
+
+    suma = num1 + num2;
+    media = suma/2.0;
+    
+    //Mostramos la media por consola
+    System.out.println("La media es: " + media);
+
+    //Cerramos escanner
+    sc.close();
+    }
+}
+    
+    
+
